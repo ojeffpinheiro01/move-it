7a86d7aad00af94dd93fb1de4ef7b3a61f21db6b
ojeffpinheiro <jefersonpinheirodesouza@gmail.com>
2021-02-24 18:52:35 -0300

    ChallengeBox

---
+++ src/components/ChallengeBox.tsx
@@ -0,0 +1,37 @@
+import styles from '../styles/components/ChallengeBox.module.css'
+
+export function ChallengeBox(){
+    const hasActiveChallenge = true
+    return(
+        <div className={styles.challengeBoxContainer}>
+            { hasActiveChallenge ? (
+                <div className={styles.challengeActive}>
+                    <header>Ganhe 400px</header>
+                    <main>
+                        <img src="icons/body.svg" alt="challenge"/>
+                        <strong>Novo desafio</strong>
+                        <p>Levante e caminhe por 3  minutos</p>
+                    </main>
+                    <footer>
+                        <button type='button' 
+                            className={styles.challengeFailedButton}>
+                                Falhei
+                            </button>
+                        <button type='button' 
+                            className={styles.challengeSucceededButton}>
+                                Completei
+                        </button>
+                    </footer>
+                </div>
+            ) : (
+                <div className={styles.challengeNotActive}>
+                <strong>Finalize um ciclo para receber um desafio</strong>
+                <p>
+                    <img src={`icons/level-up.svg`} alt="Level Up"/>
+                    Avance um level completando desafios
+                </p>
+            </div>
+            ) }
+        </div>
+    )
+}
