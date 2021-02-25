806633ccfc611c1f0273f2a7b113983845817de3
ojeffpinheiro <jefersonpinheirodesouza@gmail.com>
2021-02-25 11:55:51 -0300

    notificação web

--- src/contexts/ChallengesContext.tsx
+++ src/contexts/ChallengesContext.tsx
@@ -1,4 +1,4 @@
-import { createContext, ReactNode, useState } from 'react'
+import { createContext, ReactNode, useEffect, useState } from 'react'
 import challenges from '../../challenges.json'
 
 interface ChallengesProviderProps {
@@ -35,6 +35,10 @@ export function ChallengesProvider({ children }: ChallengesProviderProps) {
 
   const experienceToNextLevel = Math.pow((level + 1) * 4 , 2)
 
+  useEffect(() => {
+    Notification.requestPermission()
+  }, [])
+
   function levelUp() {
     setLevel(level + 1)
   }
@@ -43,6 +47,12 @@ export function ChallengesProvider({ children }: ChallengesProviderProps) {
     const randomChallengesIndex = Math.floor(Math.random() * challenges.length)
     const challenge = challenges[randomChallengesIndex]
     setActiveChallenge(challenge)
+
+    if(Notification.permission === 'granted' ){
+      new Notification('Novo desafio 🎉', {
+        body: `Valendo ${challenge.amount}xp`
+      })
+    }
 
   }
 
