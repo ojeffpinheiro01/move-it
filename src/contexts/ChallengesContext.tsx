0fdfdb8d84bff35c3749c56576765de502dd30f1
ojeffpinheiro <jefersonpinheirodesouza@gmail.com>
2021-02-25 11:58:29 -0300

    audio

--- src/contexts/ChallengesContext.tsx
+++ src/contexts/ChallengesContext.tsx
@@ -48,6 +48,8 @@ export function ChallengesProvider({ children }: ChallengesProviderProps) {
     const challenge = challenges[randomChallengesIndex]
     setActiveChallenge(challenge)
 
+    new Audio('/notification.mp3').play()
+
     if(Notification.permission === 'granted' ){
       new Notification('Novo desafio 🎉', {
         body: `Valendo ${challenge.amount}xp`
